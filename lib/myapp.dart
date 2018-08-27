bd1626bb6d61a1646c175257b423da4b3bc56ade
xiongzhuang <xiongzhuang@dcoin.com>
2018-08-27 14:58:48 +0800

    完成第一个简单列表应用

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,16 +1,18 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 //    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Startup Name Generator',
       home: new RandomWords(),
+      theme: new ThemeData(primaryColor: Colors.white),
 //      title: 'Welcome to my app',
 //      home: new Scaffold(
 //        appBar: new AppBar(
@@ -25,21 +27,20 @@ class MyApp extends StatelessWidget{
 //      ),
     );
   }
-
 }
 
-
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new RandomWordsState();
   }
-  
 }
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
 //    final wordPair = new WordPair.random();
@@ -47,20 +48,48 @@ class RandomWordsState extends State<RandomWords>{
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Startup Name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
-  
-  Widget _buildSuggestions(){
+
+  Widget _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final titles = _saved.map(
+        (wordPair) {
+          return new ListTile(
+            title: new Text(
+              wordPair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final divided =
+          ListTile.divideTiles(context: context, tiles: titles).toList();
+
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Saved Suggestions'),
+        ),
+        body: new ListView(children: divided),
+      );
+    }));
+  }
+
+  Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
       //对于每个建议的单词对都会调用异常itemBuilder，然后将单词对添加到ListTile行中
       //在偶数行，该函数会为单词对添加一个ListTitle row.
-      itemBuilder: (context, i){
-        if(i.isOdd) return new Divider();
-        final index = i ~/2;
-        if(index >= _suggestions.length){
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider(); //如果是奇数，返回分割线
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          //如果是最后一个，再添加10个
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
@@ -68,16 +97,24 @@ class RandomWordsState extends State<RandomWords>{
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
-
 }
-
-
-
