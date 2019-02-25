4f243755fe24956decbcd39e1dc4822337371e11
xiongzhuang <xiongzhuang@dcoin.com>
2019-02-25 15:59:32 +0800

    create counter

---
+++ lib/first_flutter_app/Counter.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      //应用名称
+      title: 'Flutter Demo',
+      //蓝色主题
+      theme: ThemeData(primarySwatch: Colors.blue),
+      //应用首页路由
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
